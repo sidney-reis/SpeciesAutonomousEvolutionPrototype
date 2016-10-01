1db9eb053f2b95ac21c06a4cb3ea996c297d32b4
Sidney Reis <Sidney Reis>
2016-10-01 20:30:56 -0300

    Initial Commit
    First commit with project structure and initial testing.

---
+++ SpeciesAutonomousEvolutionPrototype/Assets/CharacterMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+    private Animator anim;
+    public Rigidbody rb;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector3(0, -10, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("walking", true);
+            Vector3 position = this.transform.position;
+            position.x = (float)(position.x + 0.1);
+            position.z = (float)(position.z + 0.1);
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("walking", true);
+            Vector3 position = this.transform.position;
+            position.x = (float)(position.x - 0.1);
+            position.z = (float)(position.z + 0.1);
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("walking", true);
+            Vector3 position = this.transform.position;
+            position.x = (float)(position.x - 0.1);
+            position.z = (float)(position.z - 0.1);
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("walking", true);
+            Vector3 position = this.transform.position;
+            position.x = (float)(position.x + 0.1);
+            position.z = (float)(position.z - 0.1);
+            this.transform.position = position;
+        }
+
+        if ((!Input.GetKey(KeyCode.W)) & (!Input.GetKey(KeyCode.A)) & (!Input.GetKey(KeyCode.S)) & (!Input.GetKey(KeyCode.D)))
+        {
+            anim.SetBool("walking", false);
+        }
+    }
+}
