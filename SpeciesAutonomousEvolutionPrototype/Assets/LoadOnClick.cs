9f6e87000087917ce60796f041a720fd72ea488a
Sidney Reis <Sidney Reis>
2016-10-12 23:00:09 -0300

    Creating Menu Scene
    Selection of species with placeholder images.

---
+++ SpeciesAutonomousEvolutionPrototype/Assets/LoadOnClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick: MonoBehaviour {
+    public void LoadScene(int species)
+    {
+        SceneManager.LoadScene(1);
+        //TODO: Set species
+    }
+}
